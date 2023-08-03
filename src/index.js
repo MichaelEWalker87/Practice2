4d99715322e830b7a36ce31330eb22b6ae21c341
MichaelEWalker87 <Michael.E.Walker.87@gmail.com>
2023-08-03 14:22:48 -0600

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 1) Import the React and ReactDom libraries 
+import React from "react";
+import  ReactDOM  from 'react-dom/client';
+//  2) Get a refrance to the div with ID  root 
+const el = document.getElementById('root');
+// 3) tell React to take control of that element 
+const root = ReactDOM.createRoot(el);
+// 4) create a componnet  
+function App(){
+    const inoutType = 'number';
+    const minValue = 5;
+    return <input type={inoutType} min={minValue}/>;
+}
+// 5) show compont on scren 
+root.render(<App />);
